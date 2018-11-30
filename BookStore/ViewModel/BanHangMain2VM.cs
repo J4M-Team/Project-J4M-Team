1245dc9f5e3abd3082d2049cc72a01e6127b723c
Le Tuong Qui <qui021098@gmail.com>
2018-11-30 15:02:16 +0700

    thêm màn hình bán hàng

---
+++ BookStore/ViewModel/BanHangMain2VM.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BookStore.ViewModel
+{
+    public class BanHangMain2VM:BaseViewModel
+    {
+        private Page _FramePage;
+        public Page FramePage
+        {
+            get
+            {
+                return _FramePage;
+            }
+            set
+            {
+                if (_FramePage == value)
+                    return;
+                _FramePage = value;
+                OnPropertyChanged(nameof(FramePage));//Thêm nameof vào trước để khi rename thì sẽ thay đổi luôn cả biến không phải đổi tay khi để kiểu string "FramePage"
+            }
+        }
+
+        public ICommand DeleteCommand { get; set; }
+        public ICommand PrintCommand { get; set; }
+
+        public BanHangMain2VM()
+        {
+            DeleteCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                FramePage = new DeleteBill();
+            }
+               );
+
+            PrintCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                FramePage = new InHoaDonPage();
+            }
+               );
+        }
+    }
+}
