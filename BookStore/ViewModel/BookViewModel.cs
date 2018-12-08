c29aaa8710cec36db07e768d84baea37e0218dae
Le Tuong Qui <qui021098@gmail.com>
2018-12-08 21:02:49 +0700

    sửa lại thanh menu từ stack thành button

--- BookStore/ViewModel/BookViewModel.cs
+++ BookStore/ViewModel/BookViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -26,6 +27,24 @@ namespace BookStore.ViewModel
         }
 
         #endregion
+
+        #region properties binding
+
+        private Thickness _GridCursorMargin;
+        public Thickness GridCursorMargin
+        {
+            get
+            {
+                return _GridCursorMargin;
+            }
+            set
+            {
+                _GridCursorMargin = value;
+                OnPropertyChanged(nameof(GridCursorMargin));
+            }
+        }
+
+        #endregion
         //Biến lưu framepage
         private Page _FramePage;
         public Page FramePage
@@ -42,22 +61,19 @@ namespace BookStore.ViewModel
                 OnPropertyChanged(nameof(FramePage));//Thêm nameof vào trước để khi rename thì sẽ thay đổi luôn cả biến không phải đổi tay khi để kiểu string "FramePage"
             }
         }
-
-        public ICommand BookCommand { get; set; }//Hiển thị màn hình thống kê sách
+       
         public ICommand AccountCommand { get; set; }//Hiển thị màn hình thông tin tài khoản
         public ICommand EditBookCommand { get; set; }//Hiển thị màn hình thêm sách
         public ICommand SearchBookCommand { get; set; }//Hiển thị màn hình tìm kiếm sách
         public ICommand AccepBillCommand { get; set; }//Hiển thị màn hình duyệt hóa đơn
+        public ICommand ChartCommand { get; set; } //Hiển thị màn hình biểu đồ
+        public ICommand ExitCommand { get; set; }//Nút thoát
+
         public ICommand loadCommand { get; set; }
 
         public BookViewModel()
         {
-            BookCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
-            {
-                FramePage = new BookPage();
-            }
-               );
-
+          
             loadCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
                 if (DataTransfer.Employee_Id > 0)
@@ -67,26 +83,43 @@ namespace BookStore.ViewModel
             }
                );
 
+            ExitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                GridCursorMargin = new Thickness(0, 97 + 52 * 5, 0, 0);
+                          
+            }
+               );
+
             AccountCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
-                FramePage = new AccountPage();               
+                GridCursorMargin = new Thickness(0, 97 + 52 * 4, 0, 0);
+                FramePage = new AccountPage();
+            }
+               );
+
+            ChartCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                GridCursorMargin = new Thickness(0, 97 + 52 * 2, 0, 0);               
             }
                );
 
             EditBookCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
+                GridCursorMargin = new Thickness(0, 97 + 52 * 1, 0, 0);
                 FramePage = new EditBookPage();
             }
                );
 
             SearchBookCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
+                GridCursorMargin = new Thickness(0, 97, 0, 0);
                 FramePage = new SearchPage();
             }
               );
 
             AccepBillCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
+                GridCursorMargin = new Thickness(0, 97 + 52 * 3, 0, 0);
                 FramePage = new AccepBillPage();
             }
                );
