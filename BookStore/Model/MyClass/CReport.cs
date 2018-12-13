5945f8c4abc03d609c9f59d903b70e90499aa88c
Le Tuong Qui <qui021098@gmail.com>
2018-12-13 16:37:28 +0700

    Thêm màn hình tình hình chi tiêu của cửa hàng

--- BookStore/Model/MyClass/CReport.cs
+++ BookStore/Model/MyClass/CReport.cs
@@ -31,10 +31,15 @@ namespace BookStore.Model.MyClass
 
         #region private properties
 
-        private DateTime _Date;//Ngày cần thống kê
-        private int _CountBook;//Số lượng sách bán ra tương ứng theo ngày
+        private DateTime _Date;//Ngày,tháng cần thống kê
+        private int _CountBook;//Số lượng sách bán ra 
         private float _TotalMoney;//Tổng tiền thu được từ việc bán sách
-        private float _Profit;//Lợi nhuận trong ngày
+        private float _Profit;//Lợi nhuận 
+        private int _CountBookInput;//Tổng sách nhập vào
+        private float _TotalMoneyInput;//tiền nhập sách
+        private float _SumSalary;//Tiền lương trả cho nhân viên
+
+        //Bỏ qua tiền điện, tiền nhà, tiền dịch vụ ..............Nhiều quá
 
         #endregion
 
@@ -44,6 +49,10 @@ namespace BookStore.Model.MyClass
         public int CountBook { get { return _CountBook; } set { _CountBook = value; } }
         public float Profit { get { return _Profit; } set { _Profit = value; } }
         public float TotalMoney { get { return _TotalMoney; } set { _TotalMoney = value; } }
+        public int CountBookInput { get { return _CountBookInput; } set { _CountBookInput = value; } }
+        public float ToltalMoneyInput { get { return _TotalMoneyInput; } set { _TotalMoneyInput = value; } }
+        public float SumSalary { get { return _SumSalary; } set { _SumSalary = value; } }
+
 
         #endregion
 
@@ -140,19 +149,20 @@ namespace BookStore.Model.MyClass
                         Console.WriteLine(CountBook);
 
                         //Lấy ra tổng số tiền bán ra trong ngày
-                        float TotalMoney = (float)dataBillInfo.Sum(x => x.Price);
-                        Console.WriteLine(TotalMoney);
-
+                        float TotalMoney = 0;
+                        foreach (var item in dataBillInfo)
+                        {
+                            TotalMoney += (float)item.Price * item.Book_Count;                        
+                        }
+                        
                         //Tính toán tổng lợi nhuận dựa trên giá bán của sách trừ cho giá nhập của sách
                         float sum = 0;
                         foreach (var item in dataBillInfo)
                         {
                             //Lấy ra giá sách nhập vào với ngày cài đặt gần nhất so với ngày cần truy vấn
-                            var inputPrice = item.Book.Book_Input_Price.Where(x => x.Date_Set <= date).OrderByDescending(x => x.Date_Set).Select(x => x.Input_Price).FirstOrDefault();
-                            //Console.WriteLine($"fffffffffff{inputPrice} fsf {item.Book.Book_Name}");
-
-                            //Console.WriteLine($"{item.Price} giá bán lúc đó");
-                            sum += (float)(item.Price - inputPrice);
+                            var inputPrice = item.Book.Book_Input_Price.Where(x => x.Date_Set <= date).OrderByDescending(x => x.Date_Set).Select(x => x.Input_Price).FirstOrDefault();                            
+                            
+                            sum += (float)(item.Price - inputPrice)*item.Book_Count;//Nhân cho số lượng sách
                         }
 
                         //Tạo mới CReport
@@ -167,6 +177,65 @@ namespace BookStore.Model.MyClass
                         //Thêm vào list
                         List.Add(Report);
                     }
+                }
+            }
+            catch
+            {
+
+            }
+
+            return List;
+        }
+
+        /// <summary>
+        /// Hàm trả về báo cáo số lượng sách nhập, bán ra trong tháng, tính toán lợi nhuận trong tháng
+        /// </summary>
+        /// <returns></returns>
+        public List<CReport> ListReportAllMonth(int Year)
+        {
+            List<CReport> List = new List<CReport>();
+            try
+            {
+                //Duyệt 12 tháng trong năm
+                for (int Month = 1; Month <= 12; Month++)
+                {
+                    //Lấy ra tổng số sách nhập trong tháng
+                    int CountBookInput = DataProvider.Ins.DB.Book_Input.Where(x => SqlFunctions.DatePart("year", x.Input_Date) == Year &&
+                    SqlFunctions.DatePart("month", x.Input_Date) == Month).Select(x => x.Book_Count).DefaultIfEmpty(0).Sum();
+
+                    //Lấy ra tổng số tiền nhập sách trong tháng
+                    float TotalMoneyInput = (float)DataProvider.Ins.DB.Book_Input.Where(x => SqlFunctions.DatePart("year", x.Input_Date) == Year &&
+                    SqlFunctions.DatePart("month", x.Input_Date) == Month).Select(x => x.Book_Count * x.Input_Price).DefaultIfEmpty(0).Sum();
+
+                    //Lấy ra tổng tiền bán sách trong tháng
+                    float TotalMoney = (float)DataProvider.Ins.DB.Bill_Info.Where(x => SqlFunctions.DatePart("year", x.Bill.Bill_Date) == Year &&
+                    SqlFunctions.DatePart("month", x.Bill.Bill_Date) == Month).Select(x => x.Book_Count * x.Price).DefaultIfEmpty(0).Sum();
+
+                    //Lấy ra tổng số sách bán được trong tháng
+                    int CountBook = DataProvider.Ins.DB.Bill_Info.Where(x => SqlFunctions.DatePart("year", x.Bill.Bill_Date) == Year &&
+                    SqlFunctions.DatePart("month", x.Bill.Bill_Date) == Month).Select(x => x.Book_Count).DefaultIfEmpty(0).Sum();
+
+                    //Lấy ra tổng tiền lương trả cho nhân viên trong tháng
+                    float SumSalary = (float)DataProvider.Ins.DB.PayWage_History.Where(x => SqlFunctions.DatePart("year", x.PayWage_Date) == Year &&
+                      SqlFunctions.DatePart("month", x.PayWage_Date) == Month).Select(x => x.Employee_Salary).DefaultIfEmpty(0).Sum();
+
+                    //Tính toán lợi nhuận bằng tiền bán sách trừ cho tiền nhập sách với tiền lương trả cho nhân viên trong tháng đó
+                    float Profit = TotalMoney - TotalMoneyInput - SumSalary;
+
+                    //Tạo mới CReport
+                    CReport Report = new CReport
+                    {
+                        Date = new DateTime(Year, Month, 1),
+                        CountBook = CountBook,
+                        CountBookInput = CountBookInput,
+                        TotalMoney = TotalMoney,
+                        ToltalMoneyInput = TotalMoneyInput,
+                        SumSalary = SumSalary,
+                        Profit = Profit
+                    };
+
+                    //Thêm vào List
+                    List.Add(Report);                    
                 }
             }
             catch
--- BookStore/ViewModel/ExpenseReportPageVM.cs
+++ BookStore/ViewModel/ExpenseReportPageVM.cs
@@ -1,13 +1,87 @@
-﻿using System;
+﻿using BookStore.Model.MyClass;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BookStore.ViewModel
 {
     public class ExpenseReportPageVM:BaseViewModel
     {
+        #region data binding
 
+        private ObservableCollection<CReport> _ListReport;
+        public ObservableCollection<CReport> ListReport
+        {
+            get { return _ListReport; }
+            set
+            {
+                _ListReport = value;
+                OnPropertyChanged(nameof(ListReport));
+            }
+        }
+
+        private ObservableCollection<string> _ListYear;
+        public ObservableCollection<string> ListYear
+        {
+            get { return _ListYear; }
+            set
+            {
+                _ListYear = value;
+                OnPropertyChanged(nameof(ListYear));
+            }
+        }
+
+        private string _SelectedItemYear;
+        public string SelectedItemYear
+        {
+            get { return _SelectedItemYear; }
+            set { _SelectedItemYear = value; OnPropertyChanged(nameof(SelectedItemYear)); }
+        }
+
+        private string _TextYear;
+        public string TextYear
+        {
+            get { return _TextYear; }
+            set { _TextYear = value; OnPropertyChanged(nameof(TextYear)); }
+        }
+
+        #endregion
+
+        #region command binding
+
+        public ICommand loadCommand { get; set; }
+        public ICommand SelectionChangedYear { get; set; }
+
+        #endregion
+
+        #region method
+
+        public ExpenseReportPageVM()
+        {
+            loadCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                ListYear = new ObservableCollection<string> { "Năm 2015", "Năm 2016", "Năm 2017", "Năm 2018", "Năm 2019", "Năm 2020", "Năm 2021", "Năm 2022" };
+
+                TextYear = "Năm " + DateTime.Now.Year.ToString();
+
+                ListReport = new ObservableCollection<CReport>(CReport.Ins.ListReportAllMonth(Help.StringMonthToInt(TextYear)));
+            }
+              );
+
+            SelectionChangedYear = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                if (SelectedItemYear != null)
+                {
+                    ListReport = new ObservableCollection<CReport>(CReport.Ins.ListReportAllMonth(Help.StringMonthToInt(SelectedItemYear)));
+                }               
+            }
+              );
+        }
+
+        #endregion
     }
 }
