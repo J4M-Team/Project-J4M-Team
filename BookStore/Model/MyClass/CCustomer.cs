8fb7621af760d793f2924c6f0a6627e70f39567c
Minh Quân <dmquan@gmail.com>
2018-12-15 08:59:51 +0700

    Hàm trả thêm mới khách hàng

--- BookStore/Model/MyClass/CCustomer.cs
+++ BookStore/Model/MyClass/CCustomer.cs
@@ -86,6 +86,68 @@ namespace BookStore.Model.MyClass
             return List;
         }
 
+        /// <summary>
+        /// Hàm thêm khách hàng, thêm thành công sẽ trả về id của khách hàng đó, nếu khách hàng đã tồn tại thì trả về id khách hàng
+        /// </summary>
+        /// <param name="Customer"></param>
+        /// <returns>0 nếu thất bại, thành công trả về id khách hàng</returns>
+        public int AddCustomer(CCustomer Customer)
+        {
+            try
+            {
+                //Kiểm tra thông tin tên và số điện thoại có trống hay không
+                if (string.IsNullOrEmpty(Customer.Name) || string.IsNullOrEmpty(Customer.Phone))
+                {
+                    return 0;
+                }
+
+                //Kiểm tra xem khách hàng có trong cơ sở dữ liệu hay chưa, nếu có rồi thì trả về id của khách hàng
+                int Customer_Id = DataProvider.Ins.DB.Customers.Where(x => x.Customer_Name.ToLower() == Customer.Name.ToLower() &&
+                x.Customer_Phone.ToLower() == Customer.Phone.ToLower()).Select(x => x.Customer_Id).FirstOrDefault();
+
+                if (Customer_Id > 0)
+                {
+                    return Customer_Id;
+                }
+               
+                //Nếu chưa có thì tạo mới khách hàng
+
+                //Tạo mới customer
+
+                Customer data = new Customer
+                {
+                    Customer_Name = Customer.Name,
+                    Customer_Phone = Customer.Phone,
+                    Customer_Email = Customer.Email,
+                    Customer_Address = Customer.Address
+                };
+
+                //Thêm mới
+                DataProvider.Ins.DB.Customers.Add(data);
+
+                //Lưu thay đổi
+                DataProvider.Ins.DB.SaveChanges();
+
+                //Lấy ra id của khách hàng vừa tạo
+                //Kiểm tra xem khách hàng có trong cơ sở dữ liệu hay chưa, nếu có rồi thì trả về id của khách hàng
+                int Id = DataProvider.Ins.DB.Customers.Where(x => x.Customer_Name.ToLower() == Customer.Name.ToLower() &&
+                x.Customer_Phone.ToLower() == Customer.Phone.ToLower()).Select(x => x.Customer_Id).FirstOrDefault();
+
+                if (Id > 0)
+                {
+                    return Id;
+                }
+            }
+            catch
+            {
+
+            }
+
+            return 0;
+        }
+
+        
+
 
         #endregion
 
