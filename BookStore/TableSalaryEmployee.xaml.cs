a70c8171f9df74a6ee50af286c641c0a1db316e5
huason030298 <45062934+huason030298@users.noreply.github.com>
2018-12-17 13:08:45 +0700

    Page Salary

--- BookStore/TableSalaryEmployee.xaml.cs
+++ BookStore/TableSalaryEmployee.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace BookStore
@@ -17,7 +18,7 @@ namespace BookStore
     /// <summary>
     /// Interaction logic for TableSalaryEmployee.xaml
     /// </summary>
-    public partial class TableSalaryEmployee : Window
+    public partial class TableSalaryEmployee : Page
     {
         public TableSalaryEmployee()
         {
