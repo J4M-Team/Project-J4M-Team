3d27676270958b635e89de1f38e01f7339c59796
Le Tuong Qui <qui021098@gmail.com>
2018-12-17 15:37:04 +0700

    hiển thị lịch sử

--- BookStore/ViewModel/BillHistoryPageVM.cs
+++ BookStore/ViewModel/BillHistoryPageVM.cs
@@ -1,13 +1,51 @@
-﻿using System;
+﻿using BookStore.Model.MyClass;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BookStore.ViewModel
 {
     public class BillHistoryPageVM:BaseViewModel
     {
+        #region data binding
+        /// <summary>
+        /// Lưu List hiển thị lên List
+        /// </summary>
+        private ObservableCollection<CBill> _ListBill;
+        public ObservableCollection<CBill> ListBill
+        {
+            get { return _ListBill; }
+            set
+            {
+                _ListBill = value;
+                OnPropertyChanged(nameof(ListBill));
+            }
+        }
 
+        #endregion
+
+        #region command binding
+
+        public ICommand loadCommand { get; set; }
+
+        #endregion
+
+        #region method
+
+        public BillHistoryPageVM()
+        {
+            loadCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                ListBill = new ObservableCollection<CBill>(CBill.Ins.ListAllBill());
+                
+            }
+               );
+        }
+
+        #endregion
     }
 }
